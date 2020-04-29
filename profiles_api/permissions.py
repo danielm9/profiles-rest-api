c4272da5ba527b861b3544e2852d1881d4d1a5b2
Daniel MacRae <danielm9@uvic.ca>
2020-04-29 11:21:47 -0700

    Added user profile feed API permissions.

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         else:
             return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+                return True
+        else:
+            #return true if the user_profile.id matches the user maiking the request
+            return obj.user_profile.id == request.user.id
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,6 +10,7 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello_viewset')
 #don't need a base_name because REST figures this out from the queryset.
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
